1b3a5f1b79db16d279c5a8cd58265e6d89822ac4
Paula Gutierrez Dias <gutierrez.012900@gmail.com>
2023-10-22 13:19:53 -0500

    actualización de gitignore y conexión a la base de datos

--- src/main/java/co/safepet/clinica/ClinicaVeterinariaApplication.java
+++ src/main/java/co/safepet/clinica/ClinicaVeterinariaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClinicaVeterinariaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaVeterinariaApplication.class, args);
 	}
