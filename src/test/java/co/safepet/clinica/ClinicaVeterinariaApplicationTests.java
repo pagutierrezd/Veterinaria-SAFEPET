a01275fde62c96731cd4ac8334014a3052c1a122
Paula Gutierrez Dias <gutierrez.012900@gmail.com>
2023-10-22 12:33:17 -0500

    creación del proyecto clinica veterinaria

---
+++ src/test/java/co/safepet/clinica/ClinicaVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.safepet.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
