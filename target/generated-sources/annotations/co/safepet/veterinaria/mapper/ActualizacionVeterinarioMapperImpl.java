f0537e0fae90c6a32dc3889e1f139059cb7ec0bc
pagutierrezd <gutierrez.012900@gmail.com>
2023-11-15 09:18:44 -0500

    actualizar docker

--- target/generated-sources/annotations/co/safepet/veterinaria/mapper/ActualizacionVeterinarioMapperImpl.java
+++ target/generated-sources/annotations/co/safepet/veterinaria/mapper/ActualizacionVeterinarioMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-15T00:05:14-0500",
+    date = "2023-11-15T09:16:48-0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
 )
 @Component
